ca7e6346a84e00acba7289a79d0924e292701e62
Cevic Victor <cevizor@gmail.com>
2014-07-24 15:37:16 +0100

    new change

--- www/js/app.js
+++ www/js/app.js
@@ -1,84 +1,174 @@
+
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+var app = angular.module('hciApp', ['ionic', 'firebase', 'hciApp.controllers', 'hciApp-services',
+    'hciApp-directives', 'hciApp-filters', 'ngAnimate', 'ngTouch', 'angular-gestures']);
 
-.config(function($stateProvider, $urlRouterProvider) {
+    var PhoneGapInit = function () {
+        this.boot = function () {
+            angular.bootstrap(document, ['hciApp']);
+        };
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-    // setup an abstract state for the tabs directive
-    .state('tab', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "templates/tabs.html"
-    })
-
-    // Each tab has its own nav history stack:
-
-    .state('tab.dash', {
-      url: '/dash',
-      views: {
-        'tab-dash': {
-          templateUrl: 'templates/tab-dash.html',
-          controller: 'DashCtrl'
+        if (window.phonegap !== undefined) {
+            document.addEventListener('deviceready', function() {
+                this.boot();
+            });
+        } else {
+            console.log('PhoneGap not found, booting Angular manually');
+            this.boot();
         }
-      }
+    };
+
+    angular.element(document).ready(function() {
+        new PhoneGapInit();
+    });
+
+    app.constant('FORECASTIO_KEY', 'e5fb549e22c9c3c729ce5a5ec0c6dff7')
+    .constant('FLICKR_API_KEY', '504fd7414f6275eb5b657ddbfba80a2c')
+    .constant('AWS_ACCESS_KEY', 'AKIAIIXJM3G6BRX3W4WQ')
+    .constant('AWS_SECRETE_KEY', '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')
+    .constant('FIREBASE_URL', 'https://hcicontactmessages.firebaseio.com/')
+    .constant('KIMONOLABS', 'PBxXzZKLn1a3GJFK34ang11OgF95uY1k')
+    .filter('int', function() {
+        return function(v) {
+            return parseInt(v) || '';
+        };
     })
-
-    .state('tab.friends', {
-      url: '/friends',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
-      }
+    .run(['$rootScope','$ionicPlatform','IntroSettings','$state','$location', function($rootScope,$ionicPlatform,IntroSettings,$state,$location) {
+        $ionicPlatform.ready(function() {
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+        var intro_page_seen = IntroSettings.getSettings();
+        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+            if(toState.url == '/intro'){
+                if(intro_page_seen){
+                    console.log("there is intro page");
+                    $location.path('/app/home')
+                }
+                console.log("To state is ",toState.url)
+            }
+        });
+    }])
+    .config(function(AWSServiceProvider){
+        AWSServiceProvider.setKeys('AKIAIIXJM3G6BRX3W4WQ', 'G9ffD62jLdSLgMCzJbtjQudOf3Fj3cztP8E0Czac')
     })
-    .state('tab.friend-detail', {
-      url: '/friend/:friendId',
-      views: {
-        'tab-friends': {
-          templateUrl: 'templates/friend-detail.html',
-          controller: 'FriendDetailCtrl'
-        }
-      }
-    })
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-    .state('tab.account', {
-      url: '/account',
-      views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-account.html',
-          controller: 'AccountCtrl'
-        }
-      }
-    })
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'MenuCtrl'
+            })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+            .state('app.intro', {
+                url: '/intro',
+                views: {
+                    'menuContent':{
+                        templateUrl: 'templates/intro.html',
+                        controller: 'IntroCtrl'
+                    }
+                }
+            })
+            .state('app.home', {
+                url: "/home",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/home.html",
+                        controller: "HomeCtrl"
+                    }
+                }
+            })
 
-});
-
+            .state('app.findUs', {
+                url: "/findUs",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/findUs.html",
+                        controller: "FindUsCtrl"
+                    }
+                }
+            })
+            .state('app.todo', {
+                url: "/todo",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/podcastsLists.html",
+                        controller: "PodcastsListsCtrl"
+                    }
+                }
+            })
+            .state('app.tododetail', {
+                url: "/todo/:todoId",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/podcast.html",
+                        controller: 'PodcastsDetailCtrl'
+                    }
+                }
+            })
+            .state('app.photoGal', {
+                url: "/photoGal",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/photoGal.html",
+                        controller: "PhotoGalCtrl"
+                    }
+                }
+            })
+            .state('app.contactMessages', {
+                url: "/contactMessages",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/contactMessages.html",
+                        controller: "ContactMessagesCtrl"
+                    }
+                }
+            })
+            .state('app.contact_Message', {
+                url: "/contactMessages/:messageId",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/contact_Message.html",
+                        controller: "Contact_Message_Ctrl"
+                    }
+                }
+            })
+            .state('app.word_for_today', {
+                url: "/word_for_today",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/cards.html",
+                        controller: 'WordForTodayCtrl'
+                    }
+                }
+            })
+            .state('app.word_for_today_full', {
+                url: "/word_for_today_full",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/word_for_today.html",
+                        controller: 'WordForTodayCtrl'
+                    }
+                }
+            })
+            .state('app.send_email', {
+                url: "/send_email",
+                views: {
+                    'menuContent' :{
+                        templateUrl: "templates/send_email.html",
+                        controller: 'SendEmailCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/intro');
+    }]);
