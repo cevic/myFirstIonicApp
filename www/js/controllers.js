fd95dce1c380aa1a07ddedb7b49d6f2e99c9e911
Cevic Victor <cevizor@gmail.com>
2014-07-24 20:33:59 +0100

    more changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -32,6 +32,7 @@ appCtrl.controller('MenuCtrl', function($scope, $ionicModal, $timeout) {
     }, 1000);
   };
 });
+/*
 
 appCtrl.controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
@@ -46,3 +47,94 @@ appCtrl.controller('PlaylistsCtrl', function($scope) {
 
 appCtrl.controller('PlaylistCtrl', function($scope, $stateParams) {
 });
+*/
+
+
+appCtrl.controller('PlaylistsCtrl', ['$scope', 'AWSService', 'MyService', '$timeout', '$ionicLoading', '$filter',
+    function($scope, AWSService, MyService, $timeout, $ionicLoading, $filter){
+        /*set the funcion for showing and hiding loading screen*/
+        $scope.showLoading = function () {
+            $scope.show = $ionicLoading.show({
+                content: 'Getting Messages...',
+                showBackdrop: false
+            })
+        };
+        $scope.hide = function(){
+            $ionicLoading.hide();
+        };
+        /*List the objects and get their header metadata*/
+        var bucket = 'hci-media';
+        if(!podcasts){
+            $scope.showLoading();
+            var podcasts = [];
+            var objectAndKey = {};
+            MyService.listObjects(bucket, 'podcasts/', 'podcasts/').then(function(messages){
+                for (var i=0; i<messages.length; i++){
+                    var key = messages[i].Key;
+                    var cong = {
+                        Bucket: bucket,
+                        Key: key
+                    };
+                    (function(i){
+                        MyService.getObjHead(cong).then(function(obj){
+                            if((obj.Metadata.title !== undefined) && (obj.Metadata.title !== '') && (obj.Metadata.title!== null)){
+                                objectAndKey = {
+                                    object:obj,
+                                    objKey:messages[i].Key
+                                };
+                                podcasts.push(objectAndKey);
+                            }
+                        });
+                    })(i);
+                }
+
+            });
+        }
+        var timer = $timeout(function(){
+            $scope.messages = $filter('orderBy')(podcasts, 'object.Metadata.date', true);
+            $scope.hide();
+            $scope.predicate =  'object.Metadata.title';
+            console.log("messages ",$scope.messages);
+        }, 4000);
+        //Clean up the timer before we kill this controller
+        $scope.$on('$destroy', function() { if (timer) { $timeout.cancel(timer); } });
+        $scope.playerControl = false;
+    }]);
+
+appCtrl.controller('PlaylistCtrl', ['$scope', '$stateParams', '$filter', 'MyService', '$timeout', '$ionicLoading',
+    function($scope, $stateParams, $filter, MyService, $timeout, $ionicLoading){
+        /*set the defaults*/
+       $scope.imageSrc = null;
+
+        $scope.isPlaying = false;
+        /*set the loading show and hide functions*/
+        $scope.showLoading = function () {
+            $scope.show = $ionicLoading.show({
+                content: 'loading...',
+                showBackdrop: false
+            })
+        };
+        $scope.hide = function(){$ionicLoading.hide();};
+        $scope.showLoading();
+
+        var bucket = 'hci-media';
+        /*filter a particular object based on the ETag, then get the Key and call get object metadata. Then model the url for audio*/
+        MyService.listObjects(bucket, 'podcasts/', 'podcasts/').then(function(messages){
+            function getMessage (){
+                var messageObj = $filter('filter')(messages, {ETag: $stateParams.todoId})[0],
+                    key = messageObj.Key,
+                    params = {
+                        Bucket: bucket,
+                        Key: key
+                    };
+                MyService.getObjHead(params).then(function(obj){
+                    $scope.podcast = obj;
+                });
+                $scope.messageSrc = 'https://s3-eu-west-1.amazonaws.com/'+bucket+'/'+key;
+                $scope.hide();
+            }
+            var timer = $timeout(getMessage, 400)
+            //Clean up the timer before we kill this controller
+            $scope.$on('$destroy', function() { if (timer) { $timeout.cancel(timer); } });
+        })
+    }])
